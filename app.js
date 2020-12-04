6a55ff87421b8c51bbb8053f296b83ce752bbee9
indich98 <72091613+indich98@users.noreply.github.com>
2020-12-04 17:41:36 +0000

    Add database to blog section

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require('lodash');
 const mongoose = require("mongoose")
-//*********************************************************************
+//*******************************setup**************************************
 const app=express()
 mongoose.connect("mongodb://localhost:27017/biscuitDB", {
   useNewUrlParser: true,
@@ -18,7 +18,16 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 app.use(express.static("public"));
-//*********************************************************************
+//********************************databases*************************************
+
+const blogsSchema = new mongoose.Schema({
+  title: String,
+  content: String,
+});
+
+const Blog = mongoose.model("Blog", blogsSchema);
+
+//*********************************get/post requests************************************
  app.get("/", function(req, res){
    res.render("home");
  });
@@ -26,11 +35,58 @@ app.use(express.static("public"));
    res.render("gallery");
  });
  app.get("/blog", function(req, res){
-   res.render("blog");
+   Blog.find({},function(err,blogs){
+     res.render("blog",{
+       blogs:blogs,
+     });
+   });
+
  });
  app.get("/contact", function(req, res){
    res.render("contact");
  });
+ app.get("/compose", function(req, res){
+   res.render("composeBlog");
+ });
+
+app.post("/compose",function(req,res){
+  const blogTitle = req.body.blogTitle;
+  const blogContent = req.body.blogContent;
+
+  const blog = new Blog({
+    title: blogTitle,
+    content: blogContent,
+  });
+
+  blog.save(function(err){
+    if(!err){
+        res.redirect("/");
+    }
+});
+});
+
+
+
+ app.get("/contact", function(req, res){
+   res.render("contact");
+ });
+
+ app.get("/blogs/:blogId", function(req, res){
+   const blogId=req.params.blogId;
+
+   Blog.findOne({_id:blogId},function(err,blog){
+     if(!err){
+
+       let blogTitle=blog.title;
+       let blogContent=blog.content;
+
+       res.render("blogPage",{
+         title: blogTitle,
+         content: blogContent,
+       });
+     }
+   });
+});
 
 //*****************************************************************
 app.listen(3000, function() {
